7ea9ab9f1a9a921eb3496ab0c675c85cb6f30175
Keill Hopkins <keill.j.hopkins@gmail.com>
2022-08-02 13:06:49 +1000

    Have completed the rest of the game. An extra could be to add a reset button and an extra step between the result of the last game and resetting after 5 games. Everything seems to be working well and the scores work and evrything looks nice

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
+
+const choices = ['rock','paper','scissors'];
+let userSelection ='';
 const tiles = document.querySelectorAll(".tile");
-let userSelection;
 tiles.forEach(tile => tile.addEventListener('click',getSelected));
+
+const playButton = document.querySelector(".play");
+playButton.addEventListener('click',play); //If play had arugments then it would be ()=>{play(x,y,z)}
+
+let round = 1;
+let rounds = 5;
+let computerScore = 0;
+let userScore = 0;
+
+
 
 function getSelected(e){
     //This function adds the .seleced class to the selected tile so users can see their choice
-
-    //console.log(e.srcElement.alt); //This can be used for the user selection
     deselectTiles();
     this.classList.add("selected");
     updateSelectionText(e.srcElement.alt);
     userSelection = e.srcElement.alt;
+    return;
 }
 
 function deselectTiles(){
@@ -29,87 +40,101 @@ function updateSelectionText(selected) {
     selected = selected.charAt(0).toUpperCase() + selected.slice(1); //Capatilising the first letter
     text.innerText = `You have selected: ${selected}!`;
 }
+function getComputerChoice (){
+    //Make it pick a random number between 0 and 2. The number picked will represent the index of the choices above
+    let computer = choices[Math.floor(Math.random() * 3)];
+    return computer;
+}
 
 
 
-const choices = ['rock','paper','scissors'];
-
-        function getComputerChoice (){
-            //Make it pick a random number between 0 and 2. The number picked will represent the index of the choices above
-            let computer = choices[Math.floor(Math.random() * 3)];
-            return computer;
+function playRound (userChoice, computerChoice){
+    //returns 1: User wins
+    //returns 0: User loses
+    //returns 2: User tie
+    if (userChoice === 'rock'){
+        if (computerChoice === 'rock'){
+            return 2;
+        } else if (computerChoice === 'paper'){
+            return 0;
+        } else if (computerChoice === 'scissors'){
+            return 1;
         }
-
-        function getUserChoice (){
-            let userChoice = prompt('Enter: Rock, Paper or Scissors! ');
-            userChoice = userChoice.toLowerCase();
-            if (choices.indexOf(userChoice)=== -1){
-                console.error('This is not a valid choice!');
-                return 0;
-            }
-            return userChoice;
+    }else if (userChoice === 'paper'){
+        if (computerChoice === 'paper'){
+            return 2;
+        }else if (computerChoice === 'scissors'){
+            return 0;
+        }else if (computerChoice === 'rock'){
+            return 1;
         }
-
-        function playRound (userChoice, computerChoice){
-            //returns 1: User wins
-            //returns 0: User loses
-            //returns 2: User tie
-            if (userChoice === 'rock'){
-                if (computerChoice === 'rock'){
-                    return 2;
-                } else if (computerChoice === 'paper'){
-                    return 0;
-                } else if (computerChoice === 'scissors'){
-                    return 1;
-                }
-            }else if (userChoice === 'paper'){
-                if (computerChoice === 'paper'){
-                    return 2;
-                }else if (computerChoice === 'scissors'){
-                    return 0;
-                }else if (computerChoice === 'rock'){
-                    return 1;
-                }
-            }else if (userChoice === 'scissors'){
-                if (computerChoice === 'scissors'){
-                    return 2;
-                }else if (computerChoice === 'rock'){
-                    return 0;
-                }else if (computerChoice === 'paper'){
-                    return 1;
-                }
-            }
-            return;
+    }else if (userChoice === 'scissors'){
+        if (computerChoice === 'scissors'){
+            return 2;
+        }else if (computerChoice === 'rock'){
+            return 0;
+        }else if (computerChoice === 'paper'){
+            return 1;
         }
+    }
+}
 
-        function play(rounds){
-            let computerScore = 0;
-            let userScore = 0;
-            let userChoice = 0;
-            for (let round = 1; round <=rounds;round++){
-                while (userChoice === 0){
-                    userChoice = getUserChoice();
-                }
-                let computerChoice = getComputerChoice();
-                outcome = playRound(userChoice, computerChoice);
-                console.log(`Round ${round}:`);
-                if (outcome === 1){
-                    userScore++;
-                    console.log(`You win this round! ${userChoice} beats ${computerChoice}!`);
-                } else if (outcome === 0){
-                    computerScore++;
-                    console.log(`You lose this round! ${computerChoice} beats ${userChoice}!`)
-                } else {
-                    console.log('It is a tie! Better luck next round!')
-                }
-                console.log(`User Score: ${userScore}. Computer Score: ${computerScore}`);
-                userChoice = 0;
-            }
+function updateText () {
+    const roundText = document.querySelector('.round');
+    roundText.innerText = `Round ${round}/5:`;
+
+    const userScoreText = document.querySelector('.user-score');
+    userScoreText.innerText = `User Score: ${userScore}`;
+
+    const computerScoreText = document.querySelector('.computer-score');
+    computerScoreText.innerText = `Computer Score: ${computerScore}`;
+
+}
+
+function reset (){
+    round =1;
+    userScore = 0;
+    computerScore = 0;
+}
+
+
+
+
+function play(){
+    if (userSelection===''){return;}
+    const outcomeText = document.querySelector('.outcome');
+
+    if (round <= rounds){
+        let computerChoice = getComputerChoice();
+        outcome = playRound(userSelection, computerChoice);
+        console.log(`Round ${round}:`);
+        if (outcome === 1){
+            userScore++;
+            outcomeText.innerText = `You win this round, ${userSelection} beats ${computerChoice}!`;
+            outcomeText.style.color = 'green';
+        } else if (outcome === 0){
+            computerScore++;
+            outcomeText.innerText = `You lose this round, ${computerChoice} beats ${userSelection}!`;
+            outcomeText.style.color = 'red';
+        } else {
+            outcomeText.innerText='It is a tie! Better luck next round!';
+            outcomeText.style.color = 'yellow';
+        }
+        //console.log(`User Score: ${userScore}. Computer Score: ${computerScore}`);
+        round++;
+        deselectTiles();
+        userSelection = '';
+        updateText();
+        if (round>rounds){
             if (userScore > computerScore){
-                console.log('Congradulations you win the game!');
-            }else if (userScore < computerScore){
-                console.log('Sorry the computer has gotten you this time!');
+                outcomeText.innerText = 'Congradulations you win the game!';
+            } else if (userScore < computerScore){
+                outcomeText.innerText = 'Sorry the computer has gotten you this time!';
             } else {
-                console.log('It is a tie, try playing again!');
+                outcomeText.innerText= 'It is a tie, try playing again!';
             }
+            reset();
+            updateText();
         }
+    }
+}
